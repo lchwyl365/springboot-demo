4284fc12447f1a2397ff697a638001f1aafdbc54
超神 <lch_wyl_2012@163.com>
2018-12-10 13:21:41 +0800

    Initial commit

---
+++ src/main/java/com/team/UserController.java
@@ -0,0 +1,18 @@
+package com.team;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+    @RequestMapping("/list")
+    public String list(Model model){
+        model.addAttribute("username","liuchao");
+        return "/user/list";
+    }
+
+}
